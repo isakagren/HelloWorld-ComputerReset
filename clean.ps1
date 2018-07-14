825285b03672cb599484a0f67328e33a4ae7cd3c
Isak Ågren <isak.agren@hotmail.com>
2018-07-14 18:37:38 +0200

    Create clean.ps1

---
+++ clean.ps1
@@ -0,0 +1,49 @@
+## ###################### ##
+## Log off User Deltagare ##
+## ###################### ##
+
+Write-Output "Logging off User..."
+
+$ComputerName = "localhost"
+$UserName = "deltagare"
+foreach ($c in $ComputerName) {
+	## Find the user's session ID
+	$compArgs = "/server:$c"	
+	$whereFilter = [scriptblock]::Create("`$_ -match '$UserName'")
+	$sessions = ((quser $compArgs | Where-Object $whereFilter))
+	($sessions -split ' +')[2] | ForEach-Object {
+		logoff $_ $compArgs
+    }
+}
+
+## ##################### ##
+## Remove user deltagare ##
+## ##################### ##
+
+Write-Output "Removing User..."
+
+Remove-LocalUser -Name "deltagare"
+
+
+## ##################### ##
+## Create user deltagare ##
+## ##################### ##
+
+Write-Output "Adding User..."
+
+NET USER Deltagare "" /ADD
+
+Write-Output "Done!!!"
+
+
+## ################### ##
+## Log on to deltagare ##
+## ################### ##
+## NYI
+
+
+## ############### ##
+## Intall software ##
+## ############### ##
+## NYI
+
