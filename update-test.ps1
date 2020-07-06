766284a421f1f2b9463d1ed1ee087515ba734861
Isak Ågren <isak.agren@hotmail.com>
2020-07-06 13:54:07 +0200

    Update update-test.ps1

--- update-test.ps1
+++ update-test.ps1
@@ -2,13 +2,16 @@
 
 $user_context_folder = "C:\Users\Default\HW-User-Programs"
 $global_context_folder = "C:\HW-Global-Programs"
+$desktop_context_folder = "C:\Users\Default\Desktop"
 
 Remove-Item $user_context_folder -Force -Recurse -ErrorAction Ignore
 Remove-Item $global_context_folder -Force -Recurse -ErrorAction Ignore
+Remove-Item $desktop_context_folder -Force -Recurse -ErrorAction Ignore
 
 
 mkdir $user_context_folder
 mkdir $global_context_folder
+mkdir $desktop_context_folder
 
 
 # Download files
@@ -19,9 +22,11 @@ mkdir "C:/reset/update/downloads"
 
 $user_download = "C:\reset\update\downloads\userctx.zip"
 $global_download = "C:\reset\update\downloads\globalctx.zip"
+$desktop_download = "C:\reset\update\downloads\desktopctx.zip"
 
 $url_global = "https://github.com/isakagren/HelloWorld-ComputerReset/raw/master/globalctx_1.zip"
 $url_user = "https://github.com/isakagren/HelloWorld-ComputerReset/raw/master/userctx_1.zip"
+$url_desktop = "https://drive.google.com/file/d/1v_2ibnM0LJRwTBKr0cB5cvHPNW1tn4qQ/view?usp=sharing"
 
 
 # Download user context files
@@ -30,6 +35,9 @@ Start-BitsTransfer -Source $url_user -Destination $user_download
 # Download global context files
 Start-BitsTransfer -Source $url_global -Destination $global_download
 
+# Download desktop context files
+Start-BitsTransfer -Source $url_ddesktop -Destination $desktotp_download
+
 Remove-Item "C:\reset\config.json" -Force -ErrorAction Ignore
 Start-BitsTransfer -Source "https://raw.githubusercontent.com/isakagren/HelloWorld-ComputerReset/master/config_1.0" -Destination "C:\reset\config.json"
 
@@ -37,3 +45,4 @@ Start-BitsTransfer -Source "https://raw.githubusercontent.com/isakagren/HelloWor
 # Expand to folders
 expand-archive -Path $user_download -DestinationPath $user_context_folder
 expand-archive -Path $global_download -DestinationPath $global_context_folder
+expand-archive -Path $desktop_download -DestinationPath $desktop_context_folder
