a7e618aef1b027dbca6d35f2bc036f77bd8b2cb4
Isak Ågren <isak.agren@hotmail.com>
2020-07-06 12:03:44 +0200

    Update update-test.ps1

--- update-test.ps1
+++ update-test.ps1
@@ -1,1 +1,39 @@
-mkdir "c:/reset/update/downloads"
+# Clean up and uninstall old programs
+
+$user_context_folder = "C:\Users\Default\HW-User-Programs"
+$global_context_folder = "C:\HW-Global-Programs"
+
+Remove-Item $user_context_folder -Force -Recurse -ErrorAction Ignore
+Remove-Item $global_context_folder -Force -Recurse -ErrorAction Ignore
+
+
+mkdir $user_context_folder
+mkdir $global_context_folder
+
+
+# Download files
+
+
+Remove-Item "C:/reset/update/downloads" -Force -Recurse -ErrorAction Ignore
+mkdir "C:/reset/update/downloads"
+
+$user_download = "C:\reset\update\downloads\userctx.zip"
+$global_download = "C:\reset\update\downloads\globalctx.zip"
+
+$url_global = "https://github.com/isakagren/HelloWorld-ComputerReset/raw/master/globalctx_1.zip"
+$url_user = "https://github.com/isakagren/HelloWorld-ComputerReset/raw/master/userctx_1.zip"
+
+
+# Download user context files
+Start-BitsTransfer -Source $url_user -Destination $user_download
+
+# Download global context files
+Start-BitsTransfer -Source $url_global -Destination $global_download
+
+Remove-Item "C:\reset\config.json" -Force -ErrorAction Ignore
+Start-BitsTransfer -Source "https://raw.githubusercontent.com/isakagren/HelloWorld-ComputerReset/master/config_1.0" -Destination "C:\reset\config.json"
+
+	
+# Expand to folders
+expand-archive -Path $user_download -DestinationPath $user_context_folder
+expand-archive -Path $global_download -DestinationPath $global_context_folder
